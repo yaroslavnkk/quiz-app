a6001c9078a58dc91f705df6730075c661a8ccd8
Yaroslav <yaroslav.nikolaienko.oi.2022@lpnu.ua>
2024-06-25 17:38:40 +0300

    finished main quiz logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,9 +6,10 @@ export default function Quiz(){
     const [userAnswers, setUserAnswers] = useState([]);
     const [answerState, setAnswerState] = useState('');
     const activeQuestionId = answerState === '' ? userAnswers.length : userAnswers.length - 1;
-    const handleSelectAnswer = (function handleSelectAnswer(userAnswer){
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(userAnswer){
         setUserAnswers(prevAnswers => [...prevAnswers, userAnswer]);
         setAnswerState('answered');
+        if(userAnswer !== null){
         setTimeout(() => {
             if(userAnswer === QUESTIONS[activeQuestionId].answers[0]){
                 setAnswerState('correct');
@@ -17,34 +18,37 @@ export default function Quiz(){
             }
             setTimeout(() => {
                 setAnswerState('');
-            }, 2000);
+            }, 1000);
         }, 1000);
-    });
-    const isGameOver = activeQuestionId === QUESTIONS.length;
-    if(isGameOver === true){
-        return <QuizResult/>;
-    }
+        }else{
+            setAnswerState('');
+        }
+    }, [activeQuestionId]);
+    const isGameOver = activeQuestionId === QUESTIONS.length - 1;
     const handleSkipAnswer = useCallback(() => { handleSelectAnswer(null) }, [handleSelectAnswer]);
-    return <div id="quiz">
+    return ( isGameOver ? <QuizResult /> :
+    <div id="quiz">
         <div id="question">
-            <Timer key={activeQuestionId} timeout={5000} onTimeout={handleSkipAnswer} />
+            <Timer key={activeQuestionId} timeout={5000} onTimeout={handleSkipAnswer}/>
             <h2>{QUESTIONS[activeQuestionId].text}</h2>
             <ul id="answers">
-                    {QUESTIONS[activeQuestionId].answers.map((answer) =>{
-                     let buttonClass;
-                     const isSelected = userAnswers[userAnswers.length - 1] === answer;
-                     if(answerState === 'answered' && isSelected){
+                {QUESTIONS[activeQuestionId].answers.map((answer) => {
+                    let buttonClass;
+                    const isSelected = userAnswers[userAnswers.length - 1] === answer;
+                    if (answerState === 'answered' && isSelected) {
                         buttonClass = 'selected';
-                     }
-                     if((answerState === 'wrong' || answerState === 'correct') && isSelected){
-                         buttonClass = answerState;
-                     }
+                    }
+                    if ((answerState === 'wrong' || answerState === 'correct') && isSelected) {
+                        buttonClass = answerState;
+                    }
                     return (
                         <li key={answer} className="answer">
-                            <button className={buttonClass} onClick={() => handleSelectAnswer(answer)} key={answer}>{answer}</button>
-                        </li>)})
-                    }
+                            <button className={buttonClass} onClick={() => handleSelectAnswer(answer)}
+                                    key={answer}>{answer}</button>
+                        </li>)
+                })
+                }
             </ul>
         </div>
     </div>
-}
+)};
